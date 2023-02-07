b727bf0af9a8a29447155f951e5dd55fdb603523
samdoolan <p2672389@my365.dmu.ac.uk>
2023-02-07 16:36:13 +0000

    Added testing for address class

---
+++ Testing2/tstStaff.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+
+            //creat an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //test to see that it exists
+            Assert.IsNotNull(AStaff);
+
+            
+
+
+        }
+    }
+}
