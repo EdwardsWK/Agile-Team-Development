5f8eeee4916ec532f787ebcc287b296ce35f17b3
Semih <p2635740@my365.dmu.ac.uk>
2023-02-14 11:42:11 +0000

    Added InstanceOK test for the Product Review System

---
+++ TestingReview/tstReview.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestingReview
+{
+    [TestClass]
+    public class tstReview
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsReview AReview = new clsReview();
+
+            Assert.IsNotNull(AReview);
+        }
+    }
+}
