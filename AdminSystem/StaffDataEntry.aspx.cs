c5a5be6fffebd380ebc92e7c91cc7a7d0f323be2
samdoolan <p2672389@my365.dmu.ac.uk>
2023-03-07 11:44:54 +0000

    Staff Viewer finished

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _Staff_DataEntry : System.Web.UI.Page
 {
@@ -11,4 +12,28 @@ public partial class _Staff_DataEntry : System.Web.UI.Page
     {
 
     }
+
+    protected void btnConfirm_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStaff
+        clsStaff AStaff = new clsStaff();
+        //capture the staff details
+        AStaff.StaffID = Convert.ToInt32(txtStaffID.Text);
+        AStaff.StaffFirstName = txtFirstName.Text;
+        AStaff.StaffLastName = txtLastName.Text;
+        AStaff.StaffEmail = txtEmail.Text;
+        AStaff.StaffPassword = txtPassword.Text;
+        AStaff.StaffDateJoined = Convert.ToDateTime(txtStaffDateJoined.Text);
+        AStaff.StaffIsManager = chkIsManager.Checked;
+        //Store the staff details in a session object
+        Session["AStaff"] = AStaff;
+        //navigate to the viewer page
+        Response.Redirect("StaffViewer.aspx");
+
+
+
+
+    }
+
+
 }
