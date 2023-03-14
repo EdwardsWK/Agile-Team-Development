88ec798510d4d4a3d0cd913b347887abd778dc28
Wil K Edwards <edwardswk@protonmail.com>
2023-03-14 19:59:09 +0000

    Added data fields for the search function

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,13 +4,123 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
-        public int StaffID { get; set; }
-        public string StaffFirstName { get; set; }
-        public string StaffLastName { get; set; }
-        public string StaffEmail { get; set; }
-        public string StaffPassword { get; set; }
-        public DateTime StaffDateJoined { get; set; }
-        public bool StaffIsManager { get; set; }
+        private Int32 mStaffID;
+        public int StaffID
+        {
+            get
+            {
+                //Return the private data
+                return mStaffID;
+            }
+            set
+            {
+                //Set the private data
+                mStaffID = value;
+            }
+        }
 
+        private String mStaffFirstName;
+        public string StaffFirstName
+        {
+            get
+            {
+                //Return the private data
+                return mStaffFirstName;
+            }
+            set
+            {
+                //Set the private data
+                mStaffFirstName = value;
+            }
+        }
+
+        private String mStaffLastName;
+        public string StaffLastName
+        {
+            get
+            {
+                //Return the private data
+                return mStaffLastName;
+            }
+            set
+            {
+                //Set the private data
+                mStaffLastName = value;
+            }
+        }
+
+        private String mStaffEmail;
+        public string StaffEmail
+        {
+            get
+            {
+                //Return the private data
+                return mStaffEmail;
+            }
+            set
+            {
+                //Set the private data
+                mStaffEmail = value;
+            }
+        }
+
+        private String mStaffPassword;
+        public string StaffPassword
+        {
+            get
+            {
+                //Return the private data
+                return mStaffPassword;
+            }
+            set
+            {
+                //Set the private data
+                mStaffPassword = value;
+            }
+        }
+
+        private DateTime mStaffDateJoined;
+        public DateTime StaffDateJoined
+        {
+            get
+            {
+                //Return the private data
+                return mStaffDateJoined;
+            }
+            set
+            {
+                //Set the private data
+                mStaffDateJoined = value;
+            }
+        }
+
+        private Boolean mStaffIsManager;
+        public bool StaffIsManager
+        {
+            get
+            {
+                //Return the private data
+                return mStaffIsManager;
+            }
+            set
+            {
+                //Set the private data
+                mStaffIsManager = value;
+            }
+        }
+
+        public bool Find(int staffID)
+        {
+            //Set the private data members to test data value
+            mStaffID = 1;
+            mStaffFirstName = "FirstName";
+            mStaffLastName = "LastName";
+            mStaffEmail = "Email";
+            mStaffPassword = "Password";
+            mStaffDateJoined = Convert.ToDateTime("25/12/2022");
+            mStaffIsManager = true;
+            //always return true
+            return true;
+        }
     }
 }
