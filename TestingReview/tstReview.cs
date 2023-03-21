59a95a361d67ff2e23bb6618e1c35c0674bf7aac
Semih Ozsoz <P2635740@my365.dmu.ac.uk>
2023-03-21 15:27:44 +0000

    ClassReview

--- TestingReview/tstReview.cs
+++ TestingReview/tstReview.cs
@@ -105,5 +105,175 @@ namespace TestingReview
             Assert.AreEqual(AReview.Approved, TestData);
         }
 
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            // Create an instance of the Review class
+            clsReview AReview = new clsReview();
+            // Create Boolean variable to store the result
+            Boolean Found = false;
+            // Create a test data to use with method
+            Int32 ReviewID = 2;
+            //Invoke the method
+            Found = AReview.Find(ReviewID);
+            // Test to see that if result is true
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestReviewIDFound()
+        {
+            //create an instance of the class we want to create
+            clsReview AReview = new clsReview();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 ReviewID = 2;
+            //invoke the method
+            Found = AReview.Find(ReviewID);
+            //check the ReviewID
+            if (AReview.ReviewID != 2)
+            {
+                OK = false;
+            }
+            // test to see that the result is correct
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestReviewDateFound()
+        {
+            //create an instance of the class we want to create
+            clsReview AReview = new clsReview();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 ReviewID = 2;
+            //invoke the method
+            Found = AReview.Find(ReviewID);
+            //check the property
+            if (AReview.ReviewDate != Convert.ToDateTime("03/03/2023"))
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestCustomerIDFound()
+        {
+            //create an instance of the class we want to create
+            clsReview AReview = new clsReview();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 ReviewID = 2;
+            //invoke the method
+            Found = AReview.Find(ReviewID);
+            //check the CustomerID
+            if (AReview.CustomerID != 25)
+            {
+                OK = false;
+            }
+        }
+
+        [TestMethod]
+        public void TestProductIDFound()
+        {
+            //create an instance of the class we want to create
+            clsReview AReview = new clsReview();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 ReviewID = 2;
+            //invoke the method
+            Found = AReview.Find(ReviewID);
+            //check the value
+            if (AReview.ProductID != 10)
+            {
+                OK = false;
+            }
+        }
+
+            [TestMethod]
+
+            public void TestCommentsNoFound()
+            {
+                //create an instance of the class we want to create
+                clsReview AReview = new clsReview();
+                //boolean variable to store the result of the search
+                Boolean Found = false;
+                //boolean variable to record if data is OK (assume it is)
+                Boolean OK = true;
+                //create some test data to use with the method
+                Int32 ReviewID = 2;
+                //invoke the method
+                Found = AReview.Find(ReviewID);
+                //check the property
+                if (AReview.Comments != "Good product")
+                {
+                    OK = false;
+                }
+                //test to see that the result is correct
+                Assert.IsTrue(OK);
+            }
+
+        [TestMethod]
+
+        public void TestRatingFound()
+        {
+            //create an instance of the class we want to create
+            clsReview AReview = new clsReview();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 ReviewID = 2;
+            //invoke the method
+            Found = AReview.Find(ReviewID);
+            //check the property
+            if (AReview.Rating != 2)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+
+        public void TestApprovedFound()
+        {
+            //create an instance of the class we want to create
+            clsReview AReview = new clsReview();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 ReviewID = 2;
+            //invoke the method
+            Found = AReview.Find(ReviewID);
+            //check the property
+            if (AReview.Approved != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
     }
-}
+    }
