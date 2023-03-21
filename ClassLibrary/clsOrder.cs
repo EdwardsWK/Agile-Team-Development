2b6e22f391d9eb72de5875ae893e035708dc3e95
Wil K Edwards <edwardswk@protonmail.com>
2023-03-21 05:19:53 +0000

    Completed most tests for validation (Middle Layer) - Test for OrderTotal/Decimal still to be created

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ClassLibrary
 {
@@ -116,13 +117,13 @@ namespace ClassLibrary
             }
         }
 
-        public bool Find(int OrderID)
+        public bool Find(int orderID)
         {
             // Create an instance of the data connection
             clsDataConnection DB = new clsDataConnection();
 
             // Add the parameter for the Order ID to search for
-            DB.AddParameter("@OrderID", OrderID);
+            DB.AddParameter("@OrderID", orderID);
 
             // Execute the stored procedure
             DB.Execute("sproc_tblOrder_FilterByOrderID");
@@ -148,6 +149,105 @@ namespace ClassLibrary
                 // Return false indicating a problem
                 return false;
             }
+        }
+
+        public string Valid(string orderPlaced, string customerID, string orderNotes, string productID, string orderTotal)
+        {
+            // Create a string variable to store the error
+            String Error = "";
+
+            // Create a temporary variable to store date values
+            DateTime TempOrderPlaced;
+
+            // Create a temporary variable to store CustomerID
+            Int32 TempCustomerID;
+
+            // Create a temporary variable to store ProductID
+            Int32 TempProductID;
+
+            try
+            {
+                // Copy the orderPlaced value to the temporary variable
+                TempOrderPlaced = Convert.ToDateTime(orderPlaced);
+
+                if (TempOrderPlaced < DateTime.Now.Date)
+                {
+                    // Record the error
+                    Error = Error + "The date cannot be in the past : ";
+                }
+
+                // Check to see if the date is greater than today's date
+                if (TempOrderPlaced > DateTime.Now.Date)
+                {
+                    // Record the error
+                    Error = Error + "The date cannot be in the future : ";
+                }
+            }
+            catch
+            {
+                // Record the error
+                Error = Error + "The date was not a valid date : ";
+            }
+
+            try
+            {
+                // Copy the customerID value to the temporary variable
+                TempCustomerID = Convert.ToInt32(customerID);
+
+                if (TempCustomerID < 1)
+                {
+                    // Record the error
+                    Error = Error + "The CustomerID cannot be less than 1 : ";
+                }
+            }
+            catch (OverflowException)
+            {
+                // Handle IntegerOverflow exception
+                Error = "The CustomerID cannot be higher than 2,147,483,647: ";
+            }
+            catch (FormatException)
+            {
+                // Handle FormatException
+                Error = "The CustomerID was not a valid number : ";
+            }
+
+            // If the OrderNotes are greater than 250 characters
+            if (orderNotes.Length > 250)
+            {
+                // Record the error
+                Error = Error + "The house no must be less than 6 characters : ";
+            }
+
+            try
+            {
+                // Copy The ProductID value to the temporary variable
+                TempProductID = Convert.ToInt32(productID);
+
+                if (TempProductID < 1)
+                {
+                    // Record the error
+                    Error = Error + "The ProductID cannot be less than 1 : ";
+                }
+            }
+            catch (OverflowException)
+            {
+                // Handle IntegerOverflow exception
+                Error = "The ProductID cannot be higher than 2,147,483,647: ";
+            }
+            catch (FormatException)
+            {
+                // Handle FormatException
+                Error = "The ProductID was not a valid number : ";
+            }
+
+            /*
+             * 
+             * INSERT VALIDATION FOR ORDERTOTAL HERE
+             * 
+             */
+
+            // Return any error messages
+            return Error;
         }
     }
 }
