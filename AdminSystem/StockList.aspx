d6e2043595b36364e33524050c5f0fcb475e08df
BrandonStockManagement <P2595024@JWHX0005WIN032W>
2023-04-26 05:11:50 +0100

    tada

--- AdminSystem/StockList.aspx
+++ AdminSystem/StockList.aspx
@@ -10,8 +10,16 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:Label ID="lblFilter" runat="server" style="z-index: 1; left: 16px; top: 482px; position: absolute" Text="Enter a Product Name"></asp:Label>
             <asp:ListBox ID="lstProductList" runat="server" style="z-index: 1; left: 10px; top: 15px; position: absolute; height: 377px; width: 535px"></asp:ListBox>
         </div>
+        <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" style="z-index: 1; left: 9px; top: 401px; position: absolute" Text="Add" />
+        <asp:Button ID="btnEdit" runat="server" OnClick="btnEdit_Click" style="z-index: 1; left: 53px; top: 401px; position: absolute" Text="Update/Edit" />
+        <asp:Label ID="lblError" runat="server" style="z-index: 1; left: 12px; top: 445px; position: absolute"></asp:Label>
+        <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" style="z-index: 1; left: 159px; top: 401px; position: absolute" Text="Delete" />
+        <asp:Button ID="btnApply" runat="server" OnClick="btnApply_Click" style="z-index: 1; left: 11px; top: 512px; position: absolute" Text="Apply" />
+        <asp:Button ID="btnClear" runat="server" OnClick="btnClear_Click" style="z-index: 1; left: 79px; top: 512px; position: absolute" Text="Clear" />
+        <asp:TextBox ID="txtFilter" runat="server" style="z-index: 1; left: 204px; top: 480px; position: absolute"></asp:TextBox>
     </form>
 </body>
 </html>
--- TestingStock/tstProductCollection.cs
+++ TestingStock/tstProductCollection.cs
@@ -108,5 +108,202 @@ namespace TestingStock
             Assert.AreEqual(allProducts.Count, TestList.Count);
         }
 
+        [TestMethod]
+        public void AddMethodOK()
+        {
+            // Create an Instance of the class we want to create
+            clsProductCollection allProducts = new clsProductCollection();
+
+            // Create the item of test data
+            clsProduct TestItem = new clsProduct();
+
+            // Variable to store the primary key
+            Int32 PrimaryKey = 0;
+
+            // Set the properties
+            TestItem.ProductID = 1;
+            TestItem.ProductName = "Some Name";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.StockCount = 3;
+            TestItem.Price = 300.00f;
+            TestItem.Available = true;
+
+            // Set This.Product to the test data
+            allProducts.ThisProduct = TestItem;
+
+            // Add the record
+            PrimaryKey = allProducts.Add();
+
+            // Set the Primary key of the test data
+            TestItem.ProductID = PrimaryKey;
+
+            // Find the record
+            allProducts.ThisProduct.Find(PrimaryKey);
+
+            // Test to see that the two values are the same
+            Assert.AreEqual(allProducts.ThisProduct, TestItem);
+        }
+
+        [TestMethod]
+        public void UpdateMethodOK()
+        {
+            // Create an Instance of the class we want to create
+            clsProductCollection allProducts = new clsProductCollection();
+
+            // Create the item of test data
+            clsProduct TestItem = new clsProduct();
+
+            // Variable to store the primary key
+            Int32 PrimaryKey = 0;
+
+            // Set the properties
+            TestItem.ProductName = "Some Name";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.StockCount = 3;
+            TestItem.Price = 300.00f;
+            TestItem.Available = true;
+
+            // Set This.Product to the test data
+            allProducts.ThisProduct = TestItem;
+
+            // Add the record
+            PrimaryKey = allProducts.Add();
+
+            // Set the Primary key of the test data
+            TestItem.ProductID = PrimaryKey;
+
+            // Modify the test data
+            TestItem.ProductName = "Some Other Name";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.StockCount = 5;
+            TestItem.Price = 699.99f;
+            TestItem.Available = true;
+
+            // Set the record based on the new test data
+            allProducts.ThisProduct = TestItem;
+
+            // Update the record
+            allProducts.Update();
+
+            // Find the record
+            allProducts.ThisProduct.Find(PrimaryKey);
+
+            // Test to see that the two values are the same
+            Assert.AreEqual(allProducts.ThisProduct, TestItem);
+        }
+
+
+        [TestMethod]
+        public void DeleteMethodOK()
+        {
+            // Create an Instance of the class we want to create
+            clsProductCollection allProducts = new clsProductCollection();
+
+            // Create the item of test data
+            clsProduct TestItem = new clsProduct();
+
+            // Variable to store the primary key
+            Int32 PrimaryKey = 0;
+
+            // Set the properties
+            TestItem.ProductID = 1;
+            TestItem.ProductName = "Some Name";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.StockCount = 3;
+            TestItem.Price = 300.00f;
+            TestItem.Available = true;
+
+            // Set This.Product to the test data
+            allProducts.ThisProduct = TestItem;
+
+            // Add the record
+            PrimaryKey = allProducts.Add();
+
+            // Set the Primary key of the test data
+            TestItem.ProductID = PrimaryKey;
+
+            // Find the record
+            allProducts.ThisProduct.Find(PrimaryKey);
+
+            // Delete the record
+            allProducts.Delete();
+
+            // Find the record
+            Boolean Found = allProducts.ThisProduct.Find(PrimaryKey);
+
+            // Test to see that the two values are the same
+            Assert.AreEqual(allProducts.ThisProduct, TestItem);
+        }
+
+        [TestMethod]
+        public void ReportByProductNameMethodOK()
+        {
+            // Create an Instance of the class we want to create
+            clsProductCollection allProducts = new clsProductCollection();
+
+            // Create an instance of filtered data
+            clsProductCollection filteredProducts = new clsProductCollection();
+
+            // Apply a blank string (Should return in all records)
+            filteredProducts.ReportByProductName("");
+
+            // Test to see that the two values are the same
+            Assert.AreEqual(allProducts.Count, filteredProducts.Count);
+
+        }
+
+        [TestMethod]
+        public void ReportByProductNameNoneFound()
+        {
+
+            // Create an instance of filtered data
+            clsProductCollection filteredProducts = new clsProductCollection();
+
+            // Apply a ProductName that doesnt exist 
+            filteredProducts.ReportByProductName("XXXXXXX");
+
+            // Test to see that there are no records
+            Assert.AreEqual(0, filteredProducts.Count);
+
+        }
+
+        [TestMethod]
+        public void ReportByProductNameTestDataFound()
+        {
+
+            // Create an instance of filtered data
+            clsProductCollection filteredProducts = new clsProductCollection();
+
+            // Var to store outcome
+            Boolean OK = true;
+
+            // Apply a ProductName that doesnt exist 
+            filteredProducts.ReportByProductName("XXXXXXX");
+
+            // Check that the correct number of records are found
+            if (filteredProducts.Count == 2)
+            {
+                // Check that the first record is ID 36
+                if (filteredProducts.productList[0].ProductID != 36)
+                {
+                    OK = false;
+                }
+
+                // Check that the first record is ID 37
+                if (filteredProducts.productList[1].ProductID != 37)
+                {
+                    OK = false;
+                }
+            }
+            else
+            {
+                OK = false;
+            }
+
+            // Test to see that there are no records
+            Assert.IsTrue(OK);
+
+        }
+
     }
 }
